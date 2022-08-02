b6972c2112bd7eb1a0c1f65ed36557acb8297a9e
mokosz <kalinowski.michal.95@gmail.com>
2022-08-02 13:38:05 +0200

    check

---
+++ WTRS_Backend/src/main/java/com/kalina95/wtrs/WtrsApplication.java
@@ -0,0 +1,13 @@
+package com.kalina95.wtrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtrsApplication.class, args);
+	}
+
+}
