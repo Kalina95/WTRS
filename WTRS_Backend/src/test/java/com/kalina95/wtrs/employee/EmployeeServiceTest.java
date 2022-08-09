2c213fed692e53003cc9b42c9df05547e8618f75
mokosz <kalinowski.michal.95@gmail.com>
2022-08-09 12:24:47 +0200

    tests

---
+++ WTRS_Backend/src/test/java/com/kalina95/wtrs/employee/EmployeeServiceTest.java
@@ -0,0 +1,28 @@
+package com.kalina95.wtrs.employee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
